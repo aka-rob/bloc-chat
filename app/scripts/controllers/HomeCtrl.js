8338450fc3f42a4d93fdeb4233125a80bb9a543b
aka-rob <rmizell@gmail.com>
2017-10-07 09:28:42 -0700

    Checkpoint 4 complete

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,20 +1,23 @@
 (function() {
-  function HomeCtrl($scope, Room, $uibModal) {
-    this.rooms = Room.all;
+  function HomeCtrl($scope, Room, Message, $uibModal) {
+    $scope.rooms = Room.all;
+    // this.messages = Message.all;
 
-    this.open = function(size, template) {
+    $scope.open = function(size, template) {
       $uibModal.open({
           animation: true,
           templateUrl: 'templates/roomModal.html',
           controller: 'ModalCtrl',
           backdrop: 'static'
       });
-
     };
 
+    $scope.getByRoomId = function(roomId) {
+      $scope.messages = Message.getByRoomId(roomId);
+    }
   };
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['$scope', 'Room', '$uibModal', HomeCtrl]);
+    .controller('HomeCtrl', ['$scope', 'Room', 'Message', '$uibModal', HomeCtrl]);
 })();
---
+++ app/scripts/services/Message.js
@@ -0,0 +1,19 @@
+(function() {
+  function Message($firebaseArray) {
+    var Message = {};
+    var ref = firebase.database().ref().child('messages');
+    var messages = $firebaseArray(ref);
+
+    Message.getByRoomId = function(roomId) {
+      var query = ref.orderByChild('roomId').equalTo(roomId);
+
+      return $firebaseArray(query);
+    };
+
+    return Message;
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
+})();
